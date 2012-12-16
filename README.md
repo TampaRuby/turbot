48cde27692af6f1867d91f53c364cac664e363e5
Paul Welch <paul@pwelch.net>
2012-12-15 22:46:15 -0500

    Add nextmeetup command
    This commit adds a nextmeetup command to the bot to check for
    the next meetup via an API call.
    
    This commit also adds a basic README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Turbot
+
+A Ruby IRC bot.
+
+## Description
+
+Turbot is built using the Ruby IRC framework [cinch](https://github.com/Jberlinsky/rmeetup/)
+
+## Building
+
+```bash
+$ bundle install
+$ ruby trubot.rb
+```
---
+++ plugin/meetup.rb
@@ -0,0 +1,24 @@
+require 'rMeetup'
+
+# Use a gem to access event details from the Meetup API
+# https://github.com/Jberlinsky/rmeetup/
+
+module TurbotPlugins
+  class Meetup
+    include Cinch::Plugin
+    set :prefix, PREFIX
+    match /nextmeetup/, method: :nextmeetup
+
+    def nextmeetup(m)
+      m.reply get_meetup_info
+    rescue
+      m.reply "Sorry, something went wrong with the lookup."
+    end
+
+    def get_meetup_info
+      RMeetup::Client.api_key = MEETUP_API_KEY
+      response = RMeetup::Client.fetch(:events,{:group_urlname => "tampa-rb"})
+      "Next meeting is at #{response[0].event["venue_name"]} starting at #{response[0].event["time"]}. \n #{response[0].event["event_url"]}"
+    end
+  end
+end
