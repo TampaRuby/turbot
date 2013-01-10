d951dccf40be2652660478ef65f923ea3912a601
Jonathan Jackson <jonj@promedicalinc.com>
2013-01-10 14:15:15 -0500

    Dynamically load plugins, instead of hardcoding them.

--- turbot.rb
+++ turbot.rb
@@ -2,23 +2,27 @@ require 'cinch'
 require 'ostruct'
 require 'yaml'
 require 'rmeetup'
+require 'pry'
 
 PREFIX = /^\./
 SETTINGS = OpenStruct.new(YAML.load_file(File.expand_path('./turbot.yaml'))[:settings])
 MEETUP_API_KEY = ENV['MEETUP_API_KEY']
 
-module TurbotPlugins;end
-
-require './plugin/gem_search.rb'
-require './plugin/meetup.rb'
-require './plugin/help.rb'
+#####################################################
+# Setup plugins
+#####################################################
+dir = File.dirname(File.expand_path(__FILE__))
+Dir.chdir dir
+$: << "#{dir}/plugin"
+Dir.chdir('plugin') {Dir.glob '*.rb', &method(:require)}
+#####################################################
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server   = SETTINGS.server
-    c.channels = [SETTINGS.channel]
-    c.nick     = SETTINGS.nick
-    c.plugins.plugins = [TurbotPlugins::GemSearch, TurbotPlugins::Meetup, TurbotPlugins::Help]
+    c.server          = SETTINGS.server
+    c.channels        = [SETTINGS.channel]
+    c.nick            = SETTINGS.nick
+    c.plugins.plugins = TurbotPlugins.constants.map(&TurbotPlugins.method(:const_get))
   end
 end
 
