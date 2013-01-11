cbdfe650615ee343526428cfe193309d89fb725a
Jonathan Jackson <jonj@promedicalinc.com>
2013-01-11 10:38:01 -0500

    Add tests for turbotinfo plugin.

---
+++ spec/plugin/turbotinfo_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require 'cinch'
+
+PREFIX = /^\./
+
+require_relative '../../plugin/turbotinfo_cinch_plugin.rb'
+
+describe TurbotPlugins::TurbotInfo do
+  subject(:plugin) {TurbotPlugins::TurbotInfo.new(double.as_null_object) }
+  let(:m) { double }
+
+  context "#help" do
+    it "should return the help text for itself." do
+      m.should_receive(:reply) { |arg| arg}
+
+      plugin.help(m).should eql(".turbotinfo = Information about Turbot")
+    end
+  end
+
+  context "#turbotinfo" do
+    it "tells you how to get to turbots source." do
+      m.should_receive(:reply) { |arg| arg }
+
+      plugin.turbotinfo(m).should eql("The Turbot source code can be found here: http://git.io/turbot")
+    end
+  end
+end
