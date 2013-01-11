a64eab43dcf93980577f5f88f458b915f7232c2d
Jonathan Jackson <jonj@promedicalinc.com>
2013-01-11 10:18:18 -0500

    Finish tests for gemsearch plugin.

--- spec/plugin/gem_search_spec.rb
+++ spec/plugin/gem_search_spec.rb
@@ -4,18 +4,24 @@ require 'cinch'
 
 PREFIX = /^\./
 
-require_relative '../../plugin/gem_search.rb'
+require_relative '../../plugin/gem_search_cinch_plugin.rb'
 
 describe TurbotPlugins::GemSearch do
-  context "#gemsearch" do
-    before do
-      stub_request(:any, "http://rubygems.org/api/v1/search.json?query=mechanize").to_return([{"name" => "mechanize"}])
-    end
-    let(:fake_bot) { double.as_null_object }
-    let(:m) { stub(:reply).as_null_object }
+  subject(:plugin){ TurbotPlugins::GemSearch.new(double.as_null_object) }
+  let(:m) {double}
 
+  context "#gemsearch" do
     it "should return a list of gem names." do
-      TurbotPlugins::GemSearch.new(fake_bot).gemsearch(m, 'mechanize').should eql('mechanize')
+      stub_request(:any, "http://rubygems.org/api/v1/search.json?query=mechanize").to_return(:body => [{"name" => "mechanize"}].to_json)
+      m.should_receive(:reply) { |arg| arg }
+      plugin.gemsearch(m, 'mechanize').should eql("1 results: mechanize")
+    end
+  end
+
+  context "#help" do
+    it "should print the plugins help message." do
+      m.should_receive(:reply) { |arg| arg }
+      plugin.help(m).should eql(".gem = Gem Search")
     end
   end
 end
