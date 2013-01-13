5fe68285a2acb76fae3e3f0e7f46183b65223602
Robert Jackson <robert.w.jackson@me.com>
2013-01-13 03:02:12 -0500

    Add GithubStatus plugin.

---
+++ plugin/github_status_cinch_plugin.rb
@@ -0,0 +1,45 @@
+require 'json'
+require 'open-uri'
+require 'pry'
+
+module TurbotPlugins
+  class GithubStatus
+    include Cinch::Plugin
+
+    set :prefix, PREFIX
+
+    match /help/, :method => :help
+    def help(m)
+      m.reply ".github status = Show latest status update for Github."
+      m.reply ".github last message = Show latest manual status update for Github."
+    end
+
+    timer 300, method: :status
+    match /github status/, :method => :status
+    def status(m=nil)
+      data = get_json_data('https://status.github.com/api/status.json')
+      message = "github status: \x02#{data['last_updated']}\x02 - \x02#{data['status']}\x02"
+
+      if m
+        m.reply message
+      else
+        if data['status'] != 'good'
+          bot.channel_list.each{|c| c.send(message)}
+        end
+      end
+    end
+
+    match /github last message/, :method => :last_message
+    def last_message(m)
+      data = get_json_data('https://status.github.com/api/last-message.json')
+      m.reply "github last message: \x02#{data['created_on']}\x02 - \x02#{data['status']}\x02 - \x02#{data['body']}\x02"
+    end
+
+    private
+
+    def get_json_data(url)
+      raw = open(url).read
+      JSON.parse(raw)
+    end
+  end
+end
---
+++ spec/plugin/github_status_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+require_relative '../../plugin/github_status_cinch_plugin.rb'
+
+describe TurbotPlugins::GithubStatus do
+  subject(:plugin){ described_class.new(double.as_null_object) }
+  let(:m) {double}
+
+  context "#status" do
+    let(:reply_text) {"github status: \x022013-01-13T07:41:09Z\x02 - \x02good\x02"}
+
+    it "should reply with the current github status." do
+      m.should_receive(:reply).with(reply_text)
+      VCR.use_cassette('github_status_status'){plugin.status(m)}
+    end
+  end
+
+  context "#last_message" do
+    let(:reply_text) {"github last message: \x02\x02 - \x02good\x02 - \x02Everything operating normally.\x02"}
+
+    it "should reply with the last human github status update." do
+      m.should_receive(:reply).with(reply_text)
+      VCR.use_cassette('github_status_last_message'){plugin.last_message(m)}
+    end
+  end
+
+  context "#help" do
+    it "should send it's help info." do
+      m.should_receive(:reply).with(".github status = Show latest status update for Github.")
+      m.should_receive(:reply).with(".github last message = Show latest manual status update for Github.")
+      plugin.help(m)
+    end
+  end
+end
